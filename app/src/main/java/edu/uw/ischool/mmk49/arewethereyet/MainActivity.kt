d72f2a1aab14c28e9b5c6698e1beae78f008acd4
Mustafa <mustafamkhan28@gmail.com>
2024-11-03 01:36:34 -0700

    UI + Validation Complete

--- app/src/main/java/edu/uw/ischool/mmk49/arewethereyet/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/mmk49/arewethereyet/MainActivity.kt
@@ -1,12 +1,20 @@
 package edu.uw.ischool.mmk49.arewethereyet
 
 import android.os.Bundle
+import android.telephony.PhoneNumberFormattingTextWatcher
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var validated = false
+    lateinit var messageText: TextView
+    lateinit var phoneText: TextView
+    lateinit var intervalText: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,36 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        messageText = findViewById(R.id.editTextText)
+        phoneText = findViewById(R.id.editTextPhone2)
+        intervalText = findViewById(R.id.editTextTime)
+
+        val sendBtn = findViewById<Button>(R.id.sendBtn)
+        phoneText.addTextChangedListener(PhoneNumberFormattingTextWatcher())
+        sendBtn.setOnClickListener {
+            validated = validate()
+            if(validated) {
+                sendBtn.text = "Stop"
+                Toast.makeText(this, getString(R.string.send_toast, phoneText.text, messageText.text), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun validate () : Boolean {
+        var valid = true
+        if(messageText.text.isEmpty()) {
+            Toast.makeText(this, "Please Enter A Message", Toast.LENGTH_SHORT).show()
+            valid = false
+        }
+        if(phoneText.text.length != 14) {
+            Toast.makeText(this, "Invalid Phone Number", Toast.LENGTH_SHORT).show()
+            valid = false
+        }
+        if(intervalText.text.isEmpty()) {
+            Toast.makeText(this, "Please Enter Time", Toast.LENGTH_SHORT).show()
+            valid = false
+        }
+        return valid
     }
 }
